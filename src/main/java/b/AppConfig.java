58c6ac088cd81239b90b65bf36e582e48f6c97be
gomesuit <gomesuit@gmail.com>
2015-09-23 19:28:10 +0900

    commit

--- src/main/java/b/AppConfig.java
+++ src/main/java/b/AppConfig.java
@@ -3,14 +3,31 @@ package b;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.CronTask;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
 
 @Configuration
 @EnableScheduling
-public class AppConfig {
- 
-    @Bean
-    public MyBean bean() {
-        return new MyBean();
-    }
- 
+public class AppConfig implements SchedulingConfigurer {
+// 
+//    @Bean
+//    public MyBean bean() {
+//        return new MyBean();
+//    }
+
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		// TODO Auto-generated method stub
+		//CronTrigger CronTrigger = new CronTrigger("*/5 * * * * ?");
+		SubThread SubThread = new SubThread();
+		CronTask CronTask = new CronTask(SubThread, "*/5 * * * * ?");
+		taskRegistrar.addCronTask(CronTask);
+		
+	}
+	class SubThread implements Runnable{
+		  public void run(){
+			  System.out.println("I am called by Spring scheduler");
+		  }
+		}
 }
--- src/main/java/b/MyBean.java
+++ src/main/java/b/MyBean.java
@@ -4,7 +4,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 public class MyBean {
  
-    @Scheduled(cron="*/5 * * * * ?")
+    //@Scheduled(cron="*/5 * * * * ?")
     public void printMessage() {
         System.out.println("I am called by Spring scheduler");
     }
